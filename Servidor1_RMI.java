d2d9fbb9ecdd6a5b7853e445f62d391f81790afc
Guido Rizzo <g.rizzo29@studenti.unisa.it>
2016-10-25 20:06:32 +0100

    Appagou algunas useless pastas

--- Servidor1_RMI.java
+++
@@ -1,9 +0,0 @@
-package proj;
-public class Servidor1_RMI {
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- Servidor2_TCP.java
+++
@@ -1,9 +0,0 @@
-package proj;
-public class Servidor2_TCP {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
